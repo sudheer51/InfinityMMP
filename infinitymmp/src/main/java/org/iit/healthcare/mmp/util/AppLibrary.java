b12ab8a61c61368525586460816ede647e3b2648
prabnk <prabnk@gmail.com>
2024-03-02 22:14:15 -0500

    changing the app library for db connector

--- infinitymmp/src/main/java/org/iit/healthcare/mmp/util/AppLibrary.java
+++ infinitymmp/src/main/java/org/iit/healthcare/mmp/util/AppLibrary.java
@@ -4,6 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -74,5 +80,50 @@ public class AppLibrary {
 		return inputData;
 		
 	}
+	public static String[][] getDBValues(String uname,String pword,String dbname,String tableName,String hostip) throws ClassNotFoundException, SQLException 
+	{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+	//	Driver driver = new Driver();
+		/*
+		 * url a database url of the form jdbc:subprotocol:subnameuser 
+		 * the database user on whose behalf the connection is being madepassword 
+		 * the user's password
+		 */
+		String url="jdbc:mysql://"+hostip+":3306/"+dbname;
+		String username=uname;
+		String password=pword;
+		
+		Connection con = DriverManager.getConnection(url, username, password);
+		Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+											 ResultSet.CONCUR_READ_ONLY);
+		//int  value = stmt.executeUpdate("INSERT INTO  mmp.patient_data VALUES (12,'Joy','2021-11-12');");
+		//System.out.println("The rows are updated "+ value);
+	    stmt.executeUpdate("INSERT INTO " +dbname+"."+tableName+" VALUES " +"(13,'Jessie','2021-10-12')");
+		ResultSet rs =  stmt.executeQuery("Select * from "+dbname+"."+tableName);
+		rs.last();
+		
+		int rows = rs.getRow();
+		System.out.println("Number of rows " + rows);
+		ResultSetMetaData rsmd = rs.getMetaData();
+		int cols = rsmd.getColumnCount();
+		System.out.println("Number of cols: "+ cols);
+		
+		String [][] data= new String[rows][cols];
+		int i=0;
+		rs.beforeFirst();
+		while(rs.next())
+		{
+			for(int j=0;j<cols;j++)
+			{
+				data[i][j]=rs.getString(j+1);
+				System.out.println(data[i][j]);
+				//System.out.println("i :::"  + i +"@@@@"+"j:::::" + j);
+			}
+			System.out.println();
+			i++;
+		}	
+		return data;
+	}
+
 	
 }
